4b48ec133cc56924a30a762bb8d8f5f4f206903e
Влад Голуб <VSGolub@alfabank.ru>
2025-04-17 18:51:20 +0300

    Добавьте файлы проекта.

---
+++ CorporateRiskManagementSystem.Frontend/Pages/Audit/AuditDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CorporateRiskManagementSystem.Frontend.Pages.Admin
+{
+    public class EditUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
